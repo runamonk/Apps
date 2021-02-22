65d58d49067d4d1d78353141c4fc29bf783727e7
runamonk <runamonk@gmail.com>
2021-02-22 12:40:24 +0000

    New apps are now saved to the Apps.xml

--- Controls/AppPanel.cs
+++ Controls/AppPanel.cs
@@ -17,21 +17,15 @@ namespace Apps.Controls
         private bool IsHeader = false;
         public bool InMenu { get; set; }
         public bool InLoad { get; set; }
-               
+
         private AppMenu MenuRC;
         private ToolStripMenuItem DeleteMenuItem;
 
         XmlDocument AppsXml;
-        XmlNodeList AppsNodeList;
-
+        XmlNode AppsNode;
+        
         private string AppsXmlFilePath = Funcs.AppPath() + "\\Apps.xml";
         private string New_AppsXml_file = "<XML VERSION=\"1.0\" ENCODING=\"utf-8\">\r\n<APPS>\r\n</APPS>\r\n</XML>";
-        private string New_AppXmlNode = "<APP>\r\n" +
-                                          "<NAME>{1}</NAME>\r\n" +
-                                          "<FILENAME>{2}</FILENAME>\r\n" +
-                                          "<ARGS>{3}</ARGS>\r\n" +
-                                          "<ICON>{4}</ICON>\r\n" +
-                                        "</APP>";
 
         public AppPanel(Config myConfig, bool isHeader = false)
         {
@@ -89,7 +83,7 @@ namespace Apps.Controls
             return b;
         }
 
-        public void AddItem(string AppId, string AppName, string fileName, Image fileImage, string fileIconPath, string fileArgs, XmlNode xmlNode, int AddAtIndex)
+        public void AddItem(string AppId, string AppName, string fileName, Image fileImage, string fileIconPath, string fileArgs, int AddAtIndex)
         {
             SuspendLayout();
             AppButton b = AddAppButton();
@@ -106,10 +100,42 @@ namespace Apps.Controls
             if (fileImage != null)
                 b.FileIconImage = fileImage;           
             b.FileIconPath = fileIconPath;
-            b.MyNode = xmlNode;
+            string siblingAppId = "";
+
+            siblingAppId = ((AppButton)Controls[AddAtIndex]).AppId;
             Controls.SetChildIndex(b, AddAtIndex); // move button where we want it.
+
+            XmlNode node = AppsXml.SelectSingleNode(string.Format("//APPS//APP[@id='{0}']", b.AppId));
+            XmlNode nodeSib = null;
+
+            if ((!string.IsNullOrEmpty(siblingAppId)) && (siblingAppId != b.AppId))
+                nodeSib = AppsXml.SelectSingleNode(string.Format("//APPS//APP[@id='{0}']", siblingAppId));
             
-            //Add to AppsNodeList
+            if (node == null)
+            {
+                node = AppsXml.CreateNode(XmlNodeType.Element, "APP", null);
+                XmlAttribute XmlAtt;
+                XmlAtt = AppsXml.CreateAttribute("id");
+                XmlAtt.Value = b.AppId;
+                node.Attributes.Append(XmlAtt);
+                XmlAtt = AppsXml.CreateAttribute("appname");
+                XmlAtt.Value = AppName;
+                node.Attributes.Append(XmlAtt);
+                XmlAtt = AppsXml.CreateAttribute("filename");
+                XmlAtt.Value = fileName;
+                node.Attributes.Append(XmlAtt);
+                XmlAtt = AppsXml.CreateAttribute("fileiconpath");
+                XmlAtt.Value = fileIconPath;
+                node.Attributes.Append(XmlAtt);
+                XmlAtt = AppsXml.CreateAttribute("fileargs");
+                XmlAtt.Value = fileArgs;
+                node.Attributes.Append(XmlAtt);
+                if (nodeSib == null)
+                    AppsNode.AppendChild(node);
+                else
+                    AppsNode.InsertAfter(node, nodeSib);
+                AppsXml.Save(AppsXmlFilePath);
+            }
 
             if (OnAppAdded != null)
                 OnAppAdded();
@@ -151,8 +177,8 @@ namespace Apps.Controls
                 AppsXml.Load(AppsXmlFilePath);
             }
 
-            AppsNodeList = AppsXml.GetElementsByTagName("APPS");
-
+            AppsNode = AppsXml.SelectSingleNode("//APPS");
+            
             InLoad = false;
             if (OnAppsLoaded != null)
                 OnAppsLoaded();
@@ -168,13 +194,13 @@ namespace Apps.Controls
                 var c = ((ContextMenuStrip)((ToolStripMenuItem)sender).Owner).SourceControl;
                 if (c is AppPanel)
                 {
-                    AddItem(null, f.AppName, f.AppFileName, f.AppIconImage, f.AppIconPath, f.AppFileArgs, null, 0);
+                    AddItem(null, f.AppName, f.AppFileName, f.AppIconImage, f.AppIconPath, f.AppFileArgs, 0);
                 }
                 else
                 {
                     AppButton b = ((AppButton)(c.Parent.Parent.Parent)); // Label > Panel > Panel > AppButton
                     int i = Controls.GetChildIndex(b);
-                    AddItem(null, f.AppName, f.AppFileName, f.AppIconImage, f.AppIconPath, f.AppFileArgs, null, i);               
+                    AddItem(null, f.AppName, f.AppFileName, f.AppIconImage, f.AppIconPath, f.AppFileArgs, i);               
                 }
             }
 
