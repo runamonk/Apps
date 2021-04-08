bdd4f90d1fe9b183bd1a099fc79d0d970c84ad32
runamonk <runamonk@gmail.com>
2021-04-08 10:44:39 +0100

    Add seperator support.

---
+++ Controls/AppButtonText.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Apps.Controls
+{
+    public partial class AppButtonText : Label
+    {
+        public AppButtonText (Config myConfig)
+        {
+            AppsConfig = myConfig;
+            AppsConfig.ConfigChanged += new EventHandler(ConfigChanged);
+        }
+
+        #region Properties       
+        public bool IsSeperator { get; set; }
+        #endregion
+
+        #region Privates
+        Config AppsConfig;
+        #endregion
+
+        #region Methods
+        private void ConfigChanged(object sender, EventArgs e)
+        {
+            Invalidate(); // force a repaint.
+        }
+        #endregion
+
+        #region Overrides
+        protected override void OnPaint(PaintEventArgs e)
+        {            
+            if (!IsSeperator)
+                base.OnPaint(e);
+            else
+            {
+                Pen pen = new Pen(AppsConfig.AppsFontColor);
+                PointF pt1 = new PointF(3, Height / 2);
+                PointF pt2 = new PointF(Width-3, Height / 2);                
+                e.Graphics.DrawLine(pen, pt1, pt2);
+            }
+        }
+        #endregion
+    }
+}
