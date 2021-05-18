4a5f3d2be2546e074820dede174a251f56716400
runamonk <runamonk@gmail.com>
2021-05-18 12:55:33 +0100

    Added basic support for URLS (weblinks).

---
+++ Forms/WebLink.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace Apps.Forms
+{
+    partial class WebLink
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.EditUrlPath = new System.Windows.Forms.TextBox();
+            this.EditUrlName = new System.Windows.Forms.TextBox();
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.ButtonOK = new System.Windows.Forms.Button();
+            this.PanelRight = new System.Windows.Forms.Panel();
+            this.PanelRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "URL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(10, 9);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(35, 13);
+            this.NameLabel.TabIndex = 23;
+            this.NameLabel.Text = "Name";
+            this.NameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // EditUrlPath
+            // 
+            this.EditUrlPath.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EditUrlPath.Location = new System.Drawing.Point(47, 32);
+            this.EditUrlPath.Multiline = true;
+            this.EditUrlPath.Name = "EditUrlPath";
+            this.EditUrlPath.Size = new System.Drawing.Size(294, 40);
+            this.EditUrlPath.TabIndex = 5;
+            // 
+            // EditUrlName
+            // 
+            this.EditUrlName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.EditUrlName.Location = new System.Drawing.Point(47, 7);
+            this.EditUrlName.Name = "EditUrlName";
+            this.EditUrlName.Size = new System.Drawing.Size(294, 20);
+            this.EditUrlName.TabIndex = 0;
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonCancel.Location = new System.Drawing.Point(177, 84);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.TabIndex = 8;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            this.ButtonCancel.Click += new System.EventHandler(this.ButtonCancel_Click);
+            // 
+            // ButtonOK
+            // 
+            this.ButtonOK.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.ButtonOK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ButtonOK.Location = new System.Drawing.Point(96, 84);
+            this.ButtonOK.Name = "ButtonOK";
+            this.ButtonOK.Size = new System.Drawing.Size(75, 23);
+            this.ButtonOK.TabIndex = 7;
+            this.ButtonOK.Text = "OK";
+            this.ButtonOK.UseVisualStyleBackColor = true;
+            this.ButtonOK.Click += new System.EventHandler(this.ButtonOK_Click);
+            // 
+            // PanelRight
+            // 
+            this.PanelRight.Controls.Add(this.label1);
+            this.PanelRight.Controls.Add(this.NameLabel);
+            this.PanelRight.Controls.Add(this.EditUrlPath);
+            this.PanelRight.Controls.Add(this.EditUrlName);
+            this.PanelRight.Controls.Add(this.ButtonCancel);
+            this.PanelRight.Controls.Add(this.ButtonOK);
+            this.PanelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PanelRight.Location = new System.Drawing.Point(0, 0);
+            this.PanelRight.Name = "PanelRight";
+            this.PanelRight.Size = new System.Drawing.Size(349, 117);
+            this.PanelRight.TabIndex = 16;
+            // 
+            // WebLink
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 117);
+            this.Controls.Add(this.PanelRight);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "WebLink";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "WebLink";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.WebLink_FormClosing);
+            this.Load += new System.EventHandler(this.Form_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            this.PanelRight.ResumeLayout(false);
+            this.PanelRight.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label NameLabel;
+        private System.Windows.Forms.TextBox EditUrlName;
+        private System.Windows.Forms.Button ButtonCancel;
+        private System.Windows.Forms.Button ButtonOK;
+        private System.Windows.Forms.Panel PanelRight;
+        private System.Windows.Forms.TextBox EditUrlPath;
+    }
+}
---
+++ Forms/WebLink.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Utility;
+
+namespace Apps.Forms
+{
+    public partial class WebLink : Form
+    {
+        public WebLink(Config myConfig, AppButton appButton)
+        {
+            InitializeComponent();
+            AppsConfig = myConfig;
+            FAppButton = appButton;
+
+            BackColor = myConfig.AppsBackColor;
+            ForeColor = myConfig.AppsFontColor;
+            EditUrlName.BackColor = BackColor;
+            EditUrlName.ForeColor = ForeColor;
+            EditUrlPath.BackColor = BackColor;
+            EditUrlPath.ForeColor = ForeColor;
+            ButtonOK.BackColor = BackColor;
+            ButtonCancel.BackColor = BackColor;
+        }
+
+        #region  Properties
+        public string UrlName
+        {
+            get { return EditUrlName.Text.Trim(); }
+            set {
+                EditUrlName.Text = value;
+            }
+        }
+        public string UrlPath
+        {
+            get { return EditUrlPath.Text.Trim(); }
+            set {
+                EditUrlPath.Text = value;
+            }
+        }
+        #endregion
+
+        #region Private
+        readonly Config AppsConfig;
+        bool IsCancelled = false;
+        private readonly AppButton FAppButton;
+        private const int CP_NOCLOSE_BUTTON = 0x200;
+        #endregion
+
+        #region Events
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                ButtonOK.PerformClick();
+            else
+            if (e.KeyCode == Keys.Escape)
+                ButtonCancel.PerformClick();
+        }
+        private void Form_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(EditUrlName.Text))
+                Text = "Add URL";
+            else
+                Text = "Edit URL";
+            UrlName = FAppButton.AppName;
+            UrlPath = FAppButton.Url;
+        }
+        private void WebLink_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!IsCancelled)
+            {
+                {
+                    string ErrorStr = "";
+
+                    if (UrlName == "")
+                        ErrorStr = "Please enter a link name.";
+
+                    if (UrlPath == "")
+                        ErrorStr = (ErrorStr != "" ? ErrorStr += "\r\n" : "") + "Please enter a url.";
+
+                    if (!Funcs.IsUrl(UrlPath))
+                        ErrorStr = (ErrorStr != "" ? ErrorStr += "\r\n" : "") + "Please enter a properly formatted url. \r\n (https://www.website.com or ftp://ftp.website.com)";
+
+                    if (ErrorStr != "")
+                    {
+                        Message f = new Message(AppsConfig);
+                        f.ShowAsDialog("Error", ErrorStr);
+                        e.Cancel = true;
+                    }
+                    else
+                    {
+                        FAppButton.AppName = UrlName;
+                        FAppButton.Url = UrlPath;
+                    }
+                }
+            }
+        }
+        private void ButtonCancel_Click(object sender, EventArgs e)
+        {
+            IsCancelled = true;
+        }
+        private void ButtonOK_Click(object sender, EventArgs e)
+        {
+            IsCancelled = false;
+        }
+        #endregion
+
+        #region Overrides
+        protected override CreateParams CreateParams
+        {
+            get {
+                CreateParams myCp = base.CreateParams;
+                myCp.ClassStyle |= CP_NOCLOSE_BUTTON;
+                return myCp;
+            }
+        }
+        #endregion
+    }
+}
